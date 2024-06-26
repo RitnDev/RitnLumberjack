0e237f48949081c2f73e7c7384eedbb87023374a
RitnDev <ritn@hotmail.fr>
2024-06-26 04:00:45 +0200

    0.8.10 - migration RitnLib 0.8.2

--- modules/disco-science.lua
+++ modules/disco-science.lua
@@ -1,7 +1,7 @@
-local RitnEvent = require(ritnlib.defines.class.luaClass.event)
+
 ----------------------------------------------------------------
 local function updateDiscoScience()
-    RitnEvent.setIngredientColor("lumberjack-science-pack", {r = 0.3, g = 0.1, b = 0.0})
+    RitnLibEvent.setIngredientColor("lumberjack-science-pack", {r = 0.3, g = 0.1, b = 0.0})
 end
 ----------------------------------------------------------------
 script.on_configuration_changed(updateDiscoScience)
